d410629a78cfbbd6f5f4f774fbab7fa1198375ec
jamesparkes <parkesrjames@gmail.com>
2016-11-30 11:29:15 -0800

    add es6 template literal quiz to js_mocha examples

---
+++ examples/js_mocha/TemplateLiteral/StudentMain.js
@@ -0,0 +1,7 @@
+/*
+ * Instructions: Change the `greeting` string to use a template literal.
+ */
+
+const myName = "[NAME]";
+const greeting = "Hello, my name is " + myName;
+console.log(greeting);
