a24b6dcb416ad18da7ff2622a9f5efbddc529ce6
cameronwp <cameron.w.pittman@gmail.com>
2016-12-01 22:08:21 -0600

    updates all the pointer to smart_ptrs

--- grader-libs/cpp/include/Grader.cpp
+++ grader-libs/cpp/include/Grader.cpp
@@ -1,18 +1,40 @@
+#include <memory>
 #include "Grader.h"
 #include "RubricItem.h"
+
+using namespace std;
 
 void Grader::run()
 {
   // TODO: placeholder
+  // some kind of loop through items
 
   has_run = true;
 }
 
-void Grader::create_test()
+shared_ptr<RubricItem> Grader::create_rubric_item()
 {
-  RubricItem test;
-  // TODO: actually make the test
-  tests.push_back(test);
+  shared_ptr<RubricItem> item = make_shared<RubricItem>();
+  post_create_rubric_item(item);
+  return item;
+}
 
-  num_tests = tests.size();
+shared_ptr<RubricItem> Grader::create_rubric_item(const string& name)
+{
+  shared_ptr<RubricItem> item = make_shared<RubricItem>(name);
+  post_create_rubric_item(item);
+  return item;
+}
+
+shared_ptr<RubricItem> Grader::create_rubric_item(const function<bool()>& _callback)
+{
+  shared_ptr<RubricItem> item = make_shared<RubricItem>(_callback);
+  post_create_rubric_item(item);
+  return item;
+}
+
+void Grader::post_create_rubric_item(shared_ptr<RubricItem> item)
+{
+  items.push_back(item);
+  num_tests = items.size();
 }
