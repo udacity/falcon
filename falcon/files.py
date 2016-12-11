946451be0f65b53b71b5c9daa9de4bf1b9a0dc7c
cameronwp <cameron.w.pittman@gmail.com>
2016-12-11 16:23:55 -0600

    removes unused files

--- falcon/files.py
+++
@@ -1,9 +0,0 @@
-"""File path constants used by Falcon."""
-
-STUDENT_OUT = '.tmp/student-out.txt'
-STUDENT_ERR = '.tmp/student-err.txt'
-COMPILE_OUT = '.tmp/compile-out.txt'
-COMPILE_ERR = '.tmp/compile-err.txt'
-RESULTS_OUT = '.tmp/results-out.txt'
-RESULTS_ERR = '.tmp/results-err.txt'
-CONFIG_DIR = 'config'
