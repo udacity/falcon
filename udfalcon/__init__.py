da5ad2b7f2abec53242a56e39319400f5dbafb2d
cameronwp <cameron.w.pittman@gmail.com>
2017-01-17 19:38:20 -0600

    exposes main

--- udfalcon/__init__.py
+++ udfalcon/__init__.py
@@ -4,5 +4,10 @@ Imports the rest of the modules in the Falcon dir.
 
 from os.path import dirname, basename, isfile
 import glob
+from udfalcon.falcon import main
+
 MODULES = glob.glob(dirname(__file__)+"/*.py")
 __all__ = [basename(f)[:-3] for f in MODULES if isfile(f) and not f.startswith('_')]
+
+def fly(args={}):
+    main(args)
